a9d4680f3c49d426f4ea557802104e91ff551a86
AmanJinodiya <88363690+AmanJinodiya@users.noreply.github.com>
2023-03-01 16:35:16 +0530

    inital commit

---
+++ temp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+#define for(i,a,b) for(int i = a; i < b ; i++)
+    
+  
+int main()
+{
+     
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        //ll n; cin>>n;
+        cout<<4<<endl;
+    }
+return 0;
+}
